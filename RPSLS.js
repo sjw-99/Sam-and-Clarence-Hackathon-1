4cab4b5c9f0f7cb0bb0d4310589a8582f2f76f18
sjw-99 <samwood0312@gmail.com>
2025-11-28 10:03:55 +0000

    Created JS File for RPSLS, and added intro with rules.

---
+++ RPSLS.js
@@ -0,0 +1,13 @@
+// Log Rules to Console
+console.log("Welcome to Rock, Paper, Scissors, Lizard, Spock!");
+console.log("The rules are very simple:");
+console.log("Scissors --> Paper");
+console.log("Paper --> Rock");
+console.log("Rock --> Lizard");
+console.log("Lizard --> Spock");
+console.log("Spock --> Scissors");
+console.log("Scissors --> Lizard");
+console.log("Lizard --> Paper");
+console.log("Paper --> Spock");
+console.log("Spock --> Rock");
+console.log("Rock --> Scissors");
